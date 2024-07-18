f26a97188727854f15e333c20ff77ea12b1c0a99
kseen <korenevdn@gmail.com>
2024-07-18 18:18:15 +0300

    Ver. 0.1 Reading memory

--- src/main.rs
+++ src/main.rs
@@ -5,6 +5,7 @@ use std::ptr::{null, null_mut};
 use winapi::um::tlhelp32::{CreateToolhelp32Snapshot, Process32First, Process32Next, PROCESSENTRY32, TH32CS_SNAPPROCESS, TH32CS_SNAPMODULE, TH32CS_SNAPMODULE32, MODULEENTRY32};
 use winapi::um::handleapi::CloseHandle;
 use winapi::um::winnt::HANDLE;
+use sysinfo::System;
 
 fn get_pid(process_name: &str) -> u32 {
     unsafe {
@@ -74,6 +75,42 @@ fn get_module_base_address(pid: u32, module_name: &str) -> u64 {
     }
 }
 
+    use process_memory::*;
+
+/*
+Reads memory from the process with the given PID at the given address.
+
+# Arguments
+* `pid` - The process ID of the target process.
+* `address` - The address to read from.
+
+# Returns
+The value read from the target process's memory.
+*/
+fn read_memory<T: Copy>(pid: u32, address: u64) -> T {
+    let process_handle = pid.try_into_process_handle().unwrap();
+    let mut member = DataMember::<T>::new(process_handle);
+    member.set_offset(vec![address as usize]);
+    unsafe {
+        member.read().unwrap()
+    }
+}
+
+/*
+Writes the given value to the process with the given PID at the given address.
+
+# Arguments
+* `pid` - The process ID of the target process.
+* `address` - The address to write to.
+* `value` - The value to write.
+*/
+fn write_memory<T: Copy>(pid: u32, address: u64, value: T) {
+    let process_handle = pid.try_into_process_handle().unwrap();
+    let mut member = DataMember::<T>::new(process_handle);
+    member.set_offset(vec![address as usize]);
+    member.write(&value).unwrap();
+}
+
 
 fn main() -> std::io::Result<()> {
     let pid = get_pid("weslaymonsters.exe");
@@ -82,7 +119,7 @@ fn main() -> std::io::Result<()> {
     let base_address = get_module_base_address(pid, "weslaymonsters.exe");
     println!("Base Address: {:#x}", base_address);
     
-    use process_memory::*;
+
     let process_handle = pid.try_into_process_handle()?;
     println!("Process Handle: {:?}", process_handle);
 
@@ -94,69 +131,128 @@ fn main() -> std::io::Result<()> {
         println!("Game Points: {:?}", game_global_gold.read()?);
     }
 
-    use std::thread;
-    use std::time::Duration;
-    
-    // Read memory of the game to calculate current offset of HP
-    let known_hp_value = 100; // Example known in-game HP value
+    // Get system RAM size using sysinfo crate
+    let mut system = System::new_all();
+    system.refresh_all();
+    let ram_size = system.total_memory();
+
+    println!("System RAM size: {} bytes", ram_size);
+
+
+
     
     // Function to scan memory for the known HP value
-    fn scan_memory_for_int32_value(process_handle: &ProcessHandle, base_address: u64, known_value: u32) -> Vec<u64> {
-        let mut addresses = Vec::new();
-        addresses.reserve(10000);
-        let mut buffer = vec![0u8; 4]; // Buffer to store the read value
-        for address in (base_address..base_address + 0xF000000).step_by(4) {
-            match process_handle.copy_address(address.try_into().unwrap(), &mut buffer) {
+    fn scan_memory_for_i32(process_handle: &ProcessHandle, base_address: u64, known_value: u32, known_addresses: Vec<u64>) -> Vec<u64> {
+        let mut addresses: Vec<u64> = Vec::new();
+        addresses.reserve(200000);
+
+        if known_addresses != Vec::new() {
+            println!("Known addresses: {:?}", known_addresses.len());
+            // Search among known addresses
+            let mut buffer: Vec<u8> = vec![0u8; 4]; // Buffer to store the read values
+            let start_time = std::time::Instant::now(); // Start the timer
+            for current_address in known_addresses {
+                match process_handle.copy_address(current_address.try_into().unwrap(), &mut buffer) {
+                    Ok(_) => {
+                        let value = u32::from_ne_bytes(buffer.clone().try_into().unwrap());
+                        // println!("Value: {} ?= {}", value, known_value);
+                        if value == known_value {
+                            addresses.push(current_address);
+                        }
+
+                    }
+                    Err(_) => {}
+                }
+
+
+            }
+            let end_time = start_time.elapsed(); // Stop the timer
+            println!("[DEBUG] Search time: {:?}", end_time); // Log the search time
+            return addresses;
+        }
+        
+        let mem_cap = 0xffffffff; // Memory capacity to scan
+        let buffer_size = 0x10000; // Buffer size to read from memory
+        let mut buffer: Vec<u8> = vec![0u8; buffer_size]; // Buffer to store the read values
+        let start_time = std::time::Instant::now(); // Start the timer
+    
+        let mut current_address = base_address;
+        while current_address < base_address + mem_cap {
+            match process_handle.copy_address(current_address.try_into().unwrap(), &mut buffer) {
                 Ok(_) => {
-                    let value = u32::from_ne_bytes(buffer.clone().try_into().unwrap());
-                    if value == known_value {
-                        addresses.push(address);
+                    for offset in (0..buffer_size).step_by(4) {
+                        let value = u32::from_ne_bytes(buffer[offset..offset + 4].try_into().unwrap());
+                        if value == known_value {
+                            addresses.push(current_address + offset as u64);
+                        }
                     }
                 }
                 Err(_) => {}
             }
- 
-            let value = u32::from_ne_bytes(buffer.clone().try_into().unwrap());
-            if value == known_value {
-                addresses.push(address);
-            }
+            current_address += buffer_size as u64;
         }
+    
+        let end_time = start_time.elapsed(); // Stop the timer
+        println!("[DEBUG] Search time: {:?}", end_time); // Log the search time
         addresses
     }
     
 
+
     println!("Input current HP value: ");
-    let mut input = String::new();
+    let mut input: String = String::new();
     std::io::stdin().read_line(&mut input).unwrap();
     let known_hp_value: u32 = input.trim().parse().unwrap();
 
     // First scan
-    let first_scan_addresses = scan_memory_for_int32_value(&process_handle, base_address, known_hp_value);
+    let first_scan_addresses: Vec<u64> = scan_memory_for_i32(&process_handle, base_address, known_hp_value, Vec::new());
     println!("First scan found addresses: {:?}", first_scan_addresses.len());
     
     println!("Input new HP value: ");    
-    let mut input = String::new();
+    let mut input: String = String::new();
     std::io::stdin().read_line(&mut input).unwrap();
     let known_hp_value: u32 = input.trim().parse().unwrap();
 
     
     // Second scan to confirm the address
-    let second_scan_addresses = scan_memory_for_int32_value(&process_handle, base_address, known_hp_value);
+    let second_scan_addresses: Vec<u64> = scan_memory_for_i32(&process_handle, base_address, known_hp_value, first_scan_addresses);
     println!("Second scan found addresses: {:?}", second_scan_addresses.len());
-    
-    // Find common addresses between the two scans
-    let common_addresses: Vec<u64> = first_scan_addresses
-        .into_iter()
-        .filter(|addr| second_scan_addresses.contains(addr))
-        .collect();
-    
-    if !common_addresses.is_empty() {
-        let offset = common_addresses[0] - base_address;
-        println!("Confirmed HP value at address, Offset: {:#x}", offset);
-        println!("Count: {}", common_addresses.len());
-    } else {
-        println!("No common addresses found in the two scans.");
+
+    if second_scan_addresses.len() < 4 {
+        for address in second_scan_addresses.clone() {
+            println!("[DEBUG] offset_hp: {:#X}: {:#X}", address, read_memory::<u32>(pid, address));
+        }
     }
+
+    let offset_game_hp: u64 = second_scan_addresses[0];
+
+    // dict
+    let mut offsets: HashMap<String, u64> = HashMap::new();
+    offsets.insert("hp".to_string(), offset_game_hp);
+    offsets.insert("poison".to_string (), offset_game_hp - 0x60);
+    offsets.insert("defense".to_string(), offset_game_hp - 0x20);
+    offsets.insert("hp_max".to_string(), offset_game_hp - 0x4);
+    offsets.insert("luck".to_string(), offset_game_hp + 0x20);
+    offsets.insert("intelligence".to_string(), offset_game_hp + 0x40);
+    offsets.insert("strength".to_string(), offset_game_hp + 0x60);
+    offsets.insert("crit_chance".to_string(), offset_game_hp + 0xA0);
+    offsets.insert("xp".to_string(), offset_game_hp + 0xC0);
+    offsets.insert("gold".to_string(), offset_game_hp + 0xE0);
+    offsets.insert("spikes".to_string(), offset_game_hp + 0x100);
+
+
+    println!("HP: {}/{}", 
+        read_memory::<u32>(pid, offsets["hp"]) as u32, 
+        read_memory::<u32>(pid, offsets["hp_max"]) as u32);
+    println!("XP: {}", read_memory::<u32>(pid, offsets["xp"]) as u32);
+    println!("Gold: {}", read_memory::<u32>(pid, offsets["gold"]) as u32);
+    println!("Strength: {}", read_memory::<u32>(pid, offsets["strength"]) as u32);
+    println!("Intelligence: {}", read_memory::<u32>(pid, offsets["intelligence"]) as u32);
+    println!("Defense: {}", read_memory::<u32>(pid, offsets["defense"]) as u32);
+    println!("Poison: {}", read_memory::<u32>(pid, offsets["poison"]) as u32);
+    println!("Luck: {}", read_memory::<u32>(pid, offsets["luck"]) as u32);
+    println!("Spikes: {}", read_memory::<u32>(pid, offsets["spikes"]) as u32);
+    println!("Crit Chance: {}", read_memory::<u32>(pid, offsets["crit_chance"]) as u32);
 
     Ok(())
 }
